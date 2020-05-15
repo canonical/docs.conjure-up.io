5087d65624197f01724169e2a61cd231c4fd42ad
Thomas Bille <toto@canonical.com>
2020-05-15 11:13:01 +0100

    Update site to use multistage build

---
+++ build.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+documentation-builder --base-directory . --output-path build --output-media-path 'build/media' --media-url '/media' --tag-manager-code 'GTM-K92JCQ' --search-domain 'docs.conjure-up.io' --search-url 'https://www.ubuntu.com/search' --search-placeholder 'Search conjure-up docs' --no-link-extensions --build-version-branches --site-root https://conjure-up.io/
